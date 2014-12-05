2ad4ab8121874e21f91a3427ee2e89bf69a3c9d4
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2014-12-05 21:14:27 +0900

    bump up version number

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
 #define __VERSION_H__
 
 #define VERSION		"v.4.51.a (Oct.30th,2005)"
-#define PATCH_VERSION	"+006 (Apr.14th,2013)"
+#define PATCH_VERSION	"+007 (Dec.5th,2014)"
 
 public void Banner();
 
